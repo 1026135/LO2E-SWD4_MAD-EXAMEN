baa5e619a29bde868b351cf2cbae74f582d82182
TimBlokdijk <1026135@mborijnland.nl>
2025-06-20 08:47:55 +0200

    Installed dependencies
    npm install

--- Aapje-Alpha/screens/LoginScreen.tsx
+++ Aapje-Alpha/screens/LoginScreen.tsx
@@ -28,32 +28,3 @@ const styles = StyleSheet.create({
   input: { borderWidth: 1, marginBottom: 10, padding: 10, borderRadius: 6 },
 });
 
-// navigation/DrawerNavigator.tsx
-import React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import Dashboard from '../screens/Dashboard';
-import Actielogboek from '../screens/Actielogboek';
-import Statistieken from '../screens/Statistieken';
-import Instellingen from '../screens/Instellingen';
-import { View, Button } from 'react-native';
-
-const Drawer = createDrawerNavigator();
-
-export default function DrawerNavigator({ navigation }: any) {
-  return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Home" component={Dashboard} />
-      <Drawer.Screen name="Actielogboek" component={Actielogboek} />
-      <Drawer.Screen name="Statistieken-overzicht" component={Statistieken} />
-      <Drawer.Screen name="Instellingen" component={Instellingen} />
-      <Drawer.Screen
-        name="Logout"
-        component={() => (
-          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Button title="Bevestig uitloggen" onPress={() => navigation.replace('Login')} />
-          </View>
-        )}
-      />
-    </Drawer.Navigator>
-  );
-}
