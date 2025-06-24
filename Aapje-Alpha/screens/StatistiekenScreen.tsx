2a10e1e78e3648850c33cd911336cb853aa9c332
TimBlokdijk <1026135@mborijnland.nl>
2025-06-24 16:12:10 +0200

    Update StatistiekenScreen.tsx

--- Aapje-Alpha/screens/StatistiekenScreen.tsx
+++ Aapje-Alpha/screens/StatistiekenScreen.tsx
@@ -44,15 +44,7 @@ export default function StatistiekenScreen() {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 20,
-    },
-    center: {
-        flex: 1, justifyContent: 'center', alignItems: 'center',
-    },
-    title: {
-        fontSize: 24,
-        marginBottom: 10,
-        fontWeight: 'bold',
-    },
+    container: { padding: 20 },
+    center: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+    title: { fontSize: 24, marginBottom: 10, fontWeight: 'bold' },
 });
